b29863ef197a34a197fa4d4642af9305cf38d6c8
Piotr Kaminski <piotr@ideanest.com>
2015-12-05 15:58:59 -0800

    Switch to lodash.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _ = require('underscore');
+var _ = require('lodash');
 var esprima = require('esprima');
 var escodegen = require('escodegen');
 var estraverse = require('estraverse');
